4685af2349d51672a32e6640d0fe66bd005db616
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-03-29 04:41:38 +0600

    Add files via upload

---
+++ Possible Path.cpp
@@ -0,0 +1,76 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+llu gcd (llu a, llu b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a%b);
+}
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int tc;
+    llu a,b,x,y,g1,g2;
+
+    sf ("%d",&tc);
+
+    while (tc--)
+    {
+        sf ("%llu %llu %llu %llu",&a,&b,&x,&y);
+
+        g1 = gcd (a,b);
+        g2 = gcd (x,y);
+
+        if (g1 == g2)
+            pf ("YES");
+        else
+            pf ("NO");
+
+        pf ("\n");
+    }
+
+    return 0;
+}
