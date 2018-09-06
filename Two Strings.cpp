a8ada986ebea39a68547c6531edb0ffd60bfe4bf
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-09-07 00:01:54 +0600

    Add files via upload

---
+++ Two Strings.cpp
@@ -0,0 +1,89 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe int main (void)
+#define bair_ho return 0
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+char a[100005],b[100005];
+bool arr[30];
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    int tc,pos,n,i;
+    bool k;
+
+    sf ("%d",&tc);
+
+    for (pos=1; pos<=tc; pos++)
+    {
+        sf ("%s\n%s",a,b);
+
+        memset (arr,0,sizeof arr);
+        k = false;
+
+        for (i=0; a[i]; i++)
+            arr[a[i]-97] = 1;
+
+        for (i=0; b[i]; i++)
+        {
+            if (arr[b[i]-97] == 1)
+            {
+                k = true;
+                pf ("YES\n");
+                break;
+            }
+        }
+
+        if (!k)
+            pf ("NO\n");
+    }
+
+    bair_ho;
+}
